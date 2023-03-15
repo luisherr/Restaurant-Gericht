08aeeae62e2d1ec1f3546e4aaf3de57937b9b6b1
Luis Herrera <luis378962@gmail.com>
2023-03-15 14:46:28 -0600

    Header Finalizado

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import { images } from '../../constants'
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Persigue el nuevo sabor"/>
+      <h1 className='app__header-h1'>La clave buena mesa</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type='button' className='custom__button'>Explorar Menu</button>
+    </div>s
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
